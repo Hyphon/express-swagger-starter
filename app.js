3d6879acb4ddd62d185b969d4d1a173019a0776f
Li YiShen <309872193@qq.com>
2020-04-20 15:48:46 +0800

    fix：修复跨域问题

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express')
-const swaggerConfig = require('./config').swaggerConfig
+const cors = require('cors')
 
 const app = express()
+app.use(cors())
 
 const initRoutes = require('./routes').init
-
 initRoutes(app)
 
+const swaggerConfig = require('./config').swaggerConfig
 const expressSwagger = require('express-swagger-generator')(app)
 
 expressSwagger(swaggerConfig)
